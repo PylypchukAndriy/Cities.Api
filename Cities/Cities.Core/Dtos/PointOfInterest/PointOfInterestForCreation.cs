a4d8e6d554fc1622e6783c1ef00bf058cd8014c2
Andrii Pylypchuk <tki0074@gmail.com>
2020-08-18 12:24:16 +0300

    Added required attribute to name of POI

--- Cities/Cities.Core/Dtos/PointOfInterest/PointOfInterestForCreation.cs
+++ Cities/Cities.Core/Dtos/PointOfInterest/PointOfInterestForCreation.cs
@@ -1,7 +1,10 @@
-﻿namespace Cities.Core.Dtos.PointOfInterest
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cities.Core.Dtos.PointOfInterest
 {
     public class PointOfInterestForCreation
     {
+        [Required]
         public string Name { get; set; }
     }
 }
